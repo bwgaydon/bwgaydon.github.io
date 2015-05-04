c6af1a82d6be12fb181b583635966858bb24a741
bwgaydon <bwgaydon@users.noreply.github.com>
2015-05-04 13:45:31 -0600

    started

--- build_script.js
+++ build_script.js
@@ -1,3 +1,29 @@
 function main() {
+  var pvp = [
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-3-stalker-rush",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-naniwas-safe-expand",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-hwangsins-stargate-expand",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-dears-flexible-phoenix-expand",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-sos-dtrobo-expand",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-duckdeoks-3gate-robo",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-dears-3gate-blink",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-sases-dt-rush",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-traps-3gate-all-in",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-partings-proxy-stalker-rush",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-3gate-robo",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-traps-3gate-all-in",
+    "http://imbabuilds.com/hots-protoss/hots-pvp/pvp-4gate"
+    ];
   
+  function pvp_random() {
+    
+  }
+    
 }
+
+function random_page(array) {
+  index = Math.floor(Math.random() * array.length);
+  return index;
+}
+
+$(document).ready(main);
