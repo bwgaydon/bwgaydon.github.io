77fbe1af2456c66038d7090eb383f9c4aa431dc6
bwgaydon <bwgaydon@users.noreply.github.com>
2015-05-27 16:46:51 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -4,16 +4,6 @@ for(i=0;i<headers.length;i++) {
 }
 /*
 $(window).scroll(function () {
-    if( $(window).scrollTop() > $('#header').offset().top && !($('#header').hasClass('sticky'))) {
-        $('#header').addClass('sticky');
-    }
-    else if ($(window).scrollTop() == 0){
-        $('#header').removeClass('sticky');
-    }
-});
-*/
-
-$(window).scroll(function () {
     var header = headers[0];
     if( $(window).scrollTop() > $(header).offset().top && !($(header).hasClass('sticky'))) {
         $(header).addClass('sticky');
@@ -22,17 +12,26 @@ $(window).scroll(function () {
         $(header).removeClass('sticky');
     }
 });
-
-/*
-var i=0;
-$(window).scroll(function () {
-    //headers[i] is the header we're watching, headers[i-1] is header already stuck to top
-    while(i<headers.length) {
-        if( $(window).scrollTop() >= headers[i].offset().top) {
-            if(i!=0 && headers[i-1].hasClass('sticky')) headers[i-1].removeClass('sticky');
-            headers[i].addClass('sticky');
-            i++;
-        }
-    }
-});
 */
+
+function stick(div) {
+    var left = div.offsetLeft;
+    $(div).css({
+        "position":"absolute",
+        "top":0,
+        "left":left
+    })
+}
+
+//window.onscroll = function () {
+$(window).scroll(function () {
+    
+    var header = headers[0];    //implement multiple headers after 1 works
+    
+    if($(window).scrollTop() >= $(header).offset().top) {
+        $(header).style.position = 'absolute';
+        $(header).stick();
+        window.setTimeout(function() {stick(header)}, 16);
+    }
+    
+});
