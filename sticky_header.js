2e7255b9656ce0c2555d206a7b008172a6ad0d14
bwgaydon <bwgaydon@users.noreply.github.com>
2015-05-27 13:25:42 -0700

    refactored to handle multiple headers

--- sticky_header.js
+++ sticky_header.js
@@ -1,7 +1,25 @@
+var headers = document.getElementsByClassName('header');
+for(i=0;i<headers.length;i++) {
+    headers[i].style.zIndex = i;
+}
+/*
 $(window).scroll(function () {
-    if( $(window).scrollTop() > $('#header').offset().top && !($('#header').hasClass('sticky'))){
-    $('#header').addClass('sticky');
-    } else if ($(window).scrollTop() == 0){
-    $('#header').removeClass('sticky');
+    if( $(window).scrollTop() > $('#header').offset().top && !($('#header').hasClass('sticky'))) {
+        $('#header').addClass('sticky');
+    }
+    else if ($(window).scrollTop() == 0){
+        $('#header').removeClass('sticky');
+    }
+});
+*/
+var i=0;
+$(window).scroll(function () {
+    //headers[i] is the header we're watching, headers[i-1] is header already stuck to top
+    while(i<headers.length) {
+        if( $(window).scrollTop() >= headers[i].offset().top) {
+            if(i!=0 && headers[i-1].hasClass('sticky')) headers[i-1].removeClass('sticky');
+            headers[i].addClass('sticky');
+            i++;
+        }
     }
 });
