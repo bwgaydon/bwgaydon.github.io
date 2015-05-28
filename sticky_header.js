6cdb482a81440dad1d42ff53cdaa4f73aaa8ad14
bwgaydon <bwgaydon@users.noreply.github.com>
2015-05-28 15:05:09 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -1,6 +1,6 @@
 function main() {
-    var headers = document.getElementsByClassName('header');
-    var original_positions = [];
+    headers = document.getElementsByClassName('header');
+    original_positions = [];
     for(i=0;i<headers.length;i++) {
         original_positions[i] = headers[i].offsetTop;
     }
