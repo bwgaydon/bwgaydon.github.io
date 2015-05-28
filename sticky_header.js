dd368c88b6e6b2596169ff470b711a478d82d85e
bwgaydon <bwgaydon@users.noreply.github.com>
2015-05-28 14:32:11 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -17,6 +17,8 @@ $(window).scroll(function () {
 
 function stick(div) {
     var top = div.style.top;
+    console.log(top);
+    debugger;
     top -= 'px'
     top += ($(window).scrollTop() - div.offsetTop)
     top += 'px';
