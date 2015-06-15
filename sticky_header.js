5c10a3af604fe9683a8e9006c08ca65c1bbfdcde
bwgaydon <bwgaydon@users.noreply.github.com>
2015-06-15 13:51:46 -0700

    added $(document).ready()

--- sticky_header.js
+++ sticky_header.js
@@ -1,63 +1,69 @@
-var headers         = document.querySelectorAll('.header'),
-    lastScrollY     = 0,
-    ticking         = false;
-debugger;
+function main() {
 
+	var headers         = document.querySelectorAll('.header'),
+	    lastScrollY     = 0,
+	    ticking         = false;
+	debugger;
+	
+	
+	/**
+	 * Callback for our scroll event - just
+	 * keeps track of the last scroll value
+	 */
+	function onScroll() {
+	    lastScrollY = window.scrollY;
+	    requestTick();
+	}
+	
+	/**
+	 * Calls rAF if it's not already
+	 * been done already
+	 */
+	function requestTick() {
+	    if(!ticking) {
+	        requestAnimationFrame(update);
+	        ticking = true;
+	    }
+	}
+	
+	/**
+	 * Our animation callback
+	 */
+	function update() {
+	    var header               = null,
+	        headerTop            = [],
+	        offset               = 0;
+	
+		// first loop is going to do all
+		// the reflows (since we use offsetTop)
+	    for(var h = 0; h < headers.length; h++) {
+	
+	        header       = headers[h];
+	        headerTop[h] = header.offsetTop;
+	    }
+	
+		// second loop is going to go through
+		// the movers and add the left class
+		// to the elements' classlist
+	    for(var h = 0; h < headers.length; h++) {
+	
+	        header       = headers[h];
+	
+	        if(lastScrollY >= headerTop[h]) {
+	            header.classList.add('sticky');
+	        } else {
+	            header.classList.remove('sticky');
+	        }
+	
+	    }
+	
+		// allow further rAFs to be called
+	    ticking = false;
+	}
+	
+	// only listen for scroll events
+	window.addEventListener('scroll', onScroll, false);
 
-/**
- * Callback for our scroll event - just
- * keeps track of the last scroll value
- */
-function onScroll() {
-    lastScrollY = window.scrollY;
-    requestTick();
-}
+};
 
-/**
- * Calls rAF if it's not already
- * been done already
- */
-function requestTick() {
-    if(!ticking) {
-        requestAnimationFrame(update);
-        ticking = true;
-    }
-}
-
-/**
- * Our animation callback
- */
-function update() {
-    var header               = null,
-        headerTop            = [],
-        offset               = 0;
-
-	// first loop is going to do all
-	// the reflows (since we use offsetTop)
-    for(var h = 0; h < headers.length; h++) {
-
-        header       = headers[h];
-        headerTop[h] = header.offsetTop;
-    }
-
-	// second loop is going to go through
-	// the movers and add the left class
-	// to the elements' classlist
-    for(var h = 0; h < headers.length; h++) {
-
-        header       = headers[h];
-
-        if(lastScrollY >= headerTop[h]) {
-            header.classList.add('sticky');
-        } else {
-            header.classList.remove('sticky');
-        }
-
-    }
-
-	// allow further rAFs to be called
-    ticking = false;
-}
-
-// only listen for scroll events
-window.addEventListener('scroll', onScroll, false);
+$(document).ready(main);
