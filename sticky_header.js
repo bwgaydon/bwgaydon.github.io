2a7b51d84803c27edb1c34887dc98f9e6858488d
bwgaydon <bwgaydon@users.noreply.github.com>
2015-06-15 13:32:56 -0700

    requestAnimationFrame overhaul

--- sticky_header.js
+++ sticky_header.js
@@ -1,37 +1,63 @@
-//function main() {
-    headers = document.getElementsByClassName('header');
-    debugger;
-    original_positions = [];
-    for(i=0;i<headers.length;i++) {
-        original_positions[i] = headers[i].offsetTop;
-    }
-    //body.style.zIndex = 1;
-//}
+var headers         = document.querySelectorAll('.header'),
+    lastScrollY     = 0,
+    ticking         = false;
 
-/*
-$(window).scroll(function () {
-    var header = headers[0];
-    if( $(window).scrollTop() > $(header).offset().top && !($(header).hasClass('sticky'))) {
-        $(header).addClass('sticky');
-    }
-    else if ($(window).scrollTop() == 0){
-        $(header).removeClass('sticky');
-    }
-});
-*/
 
-function stick(div) {
-    div.style.top = document.body.scrollTop + 'px';
+/**
+ * Callback for our scroll event - just
+ * keeps track of the last scroll value
+ */
+function onScroll() {
+    lastScrollY = window.scrollY;
+    requestTick();
 }
 
-
-window.onscroll = function () {
-    var header = headers[0];    //implement multiple headers after 1 works
-    var orig = original_positions[0];
-    if(document.body.scrollTop > orig) {
-        stick(header);
+/**
+ * Calls rAF if it's not already
+ * been done already
+ */
+function requestTick() {
+    if(!ticking) {
+        requestAnimationFrame(update);
+        ticking = true;
     }
-};
+}
 
-//$(document).ready(main());
+/**
+ * Our animation callback
+ */
+function update() {
+    var header               = null,
+        headerTop            = [],
+        halfWindowHeight     = window.innerHeight * 0.5,
+        offset               = 0;
 
+	// first loop is going to do all
+	// the reflows (since we use offsetTop)
+    for(var h = 0; h < headers.length; h++) {
+
+        header       = headers[h];
+        headerTop[h] = header.offsetTop;
+    }
+
+	// second loop is going to go through
+	// the movers and add the left class
+	// to the elements' classlist
+    for(var h = 0; h < headers.length; h++) {
+
+        header       = headers[h];
+
+        if(lastScrollY > headerTop[h] - halfWindowHeight) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+
+    }
+
+	// allow further rAFs to be called
+    ticking = false;
+}
+
+// only listen for scroll events
+window.addEventListener('scroll', onScroll, false);
