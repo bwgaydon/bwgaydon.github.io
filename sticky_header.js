3de08decea12ab88b2d442b26aa9426ed5609338
bwgaydon <bwgaydon@users.noreply.github.com>
2015-06-16 15:33:45 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -1,14 +1,20 @@
 function main() {
 
 	headers = document.querySelectorAll('.header'),
-	index = 0;
+	i = 0;	//header index
 	
 	//size overflow pane
-	overflow_pane = document.getElementById('overflow-pane');
+	overflow_pane = document.getElementById('overflow_pane');
+	overflow_header = document.getElementById('overflow_header');
 	
 	function onScroll() {
 		//if div reaches top of page
 		//copy div into overflow pane
+		if(window.scrollY >= headers[i].offsetTop) {
+			overflow_header.innerHTML = headers[i].innerHTML;
+			headers[i].style.display = none;
+			i++;
+		}
 	}
 	
 	// only listen for scroll events
