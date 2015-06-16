3639307621033cc0b107763bbb80d3e65ee26157
bwgaydon <bwgaydon@users.noreply.github.com>
2015-06-16 13:07:43 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -1,71 +1,12 @@
 function main() {
 
 	headers = document.querySelectorAll('.header'),
-	lastScrollY = 0,
-	ticking = false;
+	index = 0;
 	
-	
-	/**
-	 * Callback for our scroll event - just
-	 * keeps track of the last scroll value
-	 */
 	function onScroll() {
-	    /*
-	    lastScrollY = window.scrollY;
-	    requestTick();
-	    */
-	    do {
-		lastScrollY = window.scrollY;
-		window.setTimeout(function() {
-	    	    newScrollY = window.scrollY;
-		}, 100);
-	    } while(lastScrollY != newScrollY);
-	    requestTick();
+		//if div reaches top of page
+		//copy div into overflow pane
 	}
-	
-	/**
-	 * Calls rAF if it's not already
-	 * been done already
-	 */
-	function requestTick() {
-	    if(!ticking) {
-	        requestAnimationFrame(update);
-	        ticking = true;
-	    }
-	}
-	
-	/**
-	 * Our animation callback
-	 */
-	function update() {
-	    var header = null,
-	        headerTop = [],
-	        offset = 0;
-	
-		// first loop is going to do all
-		// the reflows (since we use offsetTop)
-	    for(var h = 0; h < headers.length; h++) {
-	
-	        header = headers[h];
-	        headerTop[h] = header.offsetTop;
-	    }
-	
-		// second loop is going to go through
-		// the movers and add the left class
-		// to the elements' classlist
-	    for(var h = 0; h < headers.length; h++) {
-	
-	        header = headers[h];
-		var calc = lastScrollY - headerTop[h];
-		console.log("calc = " + calc);
-            	header.style.top = lastScrollY + "px";
-	
-	    }
-	
-		// allow further rAFs to be called
-	    ticking = false;
-	}
-	
 	
 	// only listen for scroll events
 	window.addEventListener('scroll', onScroll, false);
