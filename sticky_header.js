bb0844e0128dfd0205d565d8440485b84b6c4189
bwgaydon <bwgaydon@users.noreply.github.com>
2015-06-16 13:47:08 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -3,6 +3,11 @@ function main() {
 	headers = document.querySelectorAll('.header'),
 	index = 0;
 	
+	//size overflow pane
+	overflow_pane = document.getElementById('overflow_pane');
+	overflow_pane.style.height = window.height;
+	overflow_pane.style.width = window.width;
+	
 	function onScroll() {
 		//if div reaches top of page
 		//copy div into overflow pane
