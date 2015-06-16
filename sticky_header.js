7c7ff61df561e1487f700effc412c8f5a0caeb1d
bwgaydon <bwgaydon@users.noreply.github.com>
2015-06-16 16:48:18 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -21,7 +21,7 @@ function main() {
 	}
 	
 	// only listen for scroll events
-	document.getElementById('body').addEventListener('scroll', onScroll, false);
+	window.addEventListener('scroll', onScroll, false);
 	
 	$('.container-div')[0].style.height=window.innerHeight+'px';
 
