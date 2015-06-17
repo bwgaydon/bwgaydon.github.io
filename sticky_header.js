70ffaef91aabfd76b0a36afedf2b990ad243e2f6
bwgaydon <bwgaydon@users.noreply.github.com>
2015-06-17 12:40:29 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -12,7 +12,7 @@ function main() {
 	}
 	
 	function prev_header() {
-		if(i>0 && i < headers.length-1) return headers[i-1];
+		if(i>0 && i < headers.length) return headers[i-1];
 		else if(i == headers.length-1) return headers[i];
 		else return null;
 	}
@@ -28,7 +28,7 @@ function main() {
 			sticky.style.backgroundColor = getComputedStyle(next).backgroundColor;
 			next.style.visibility = 'hidden';
 			sticky.style.display = 'block';
-			if(headers.length != i+1) i++;
+			if(i < headers.length) i++;
 		}
 		if(container.scrollTop <= prev_header().offsetTop) {
 			//replace header in its place
