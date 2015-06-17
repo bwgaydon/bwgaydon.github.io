0ce1d74efedcf2cd8d2cab5094e748a2bd98d962
bwgaydon <bwgaydon@users.noreply.github.com>
2015-06-17 11:42:09 -0700

    Update sticky_header.js

--- sticky_header.js
+++ sticky_header.js
@@ -3,28 +3,30 @@ function main() {
 	headers = document.getElementsByClassName('header'),
 	i = 0;	//header index
 	
-	overflow_header = document.getElementById('overflow_header');
+	sticky = document.getElementById('overflow_header');
 	container = document.getElementsByClassName('container')[0];
 	
-	function stick(source, target) {
-		target.innerHTML = source.innerHTML;
-		target.style.backgroundColor = getComputedStyle(source).backgroundColor;
-		source.style.visibility = 'hidden';
-		target.style.display = 'block';
-	}
 	
 	function onScroll() {
 		//if div reaches top of page
 		//copy div into overflow pane
 		
 		if(container.scrollTop >= headers[i].offsetTop) {
-			stick(headers[i], overflow_header);
+			sticky.innerHTML = source.innerHTML;
+			sticky.style.backgroundColor = getComputedStyle(headers[i]).backgroundColor;
+			headers[i].style.visibility = 'hidden';
+			sticky.style.display = 'block';
 			if(headers.length != i+1) i++;
 		}
+		/*
 		if(container.scrollTop <= headers[i-1].offsetTop) {
-			stick(headers[i], overflow_header);
+			target.innerHTML = source.innerHTML;
+			target.style.backgroundColor = getComputedStyle(source).backgroundColor;
+			source.style.display = 'none';
+			target.style.visibility = 'visible';
 			if(i != 0) i--;
 		}
+		*/
 	}
 	
 	// only listen for scroll events
