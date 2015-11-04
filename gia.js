7a419a538c3e304dee84a4dfb92ce0d74a598dbd
bwgaydon <bwgaydon@users.noreply.github.com>
2015-11-03 22:51:54 -0700

    Update gia.js

--- gia.js
+++ gia.js
@@ -2,9 +2,6 @@ function events() {
     document.getElementById('login_button').addEventListener('click', login_animate);
     document.getElementById('signup_button').addEventListener('click', signup_animate);
     inputbox = document.getElementsByClassName("inputbox");
-    for(var i=0;i<inputbox.length;i++){
-        inputbox[i].addEventListener('click', clearBox(inputbox[i]), false);
-    }
 }
 
 function login_animate() {
