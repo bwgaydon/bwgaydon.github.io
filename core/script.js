58dfc4260e1621c10c465dc3ad89dd6cc8dcf42e
bwgaydon <bwgaydon@users.noreply.github.com>
2016-01-22 15:12:19 -0700

    Update script.js

--- core/script.js
+++ core/script.js
@@ -1,11 +1,3 @@
-function onLoad() {
-	$("#metrics").addEventListener("focus", function(){
-  	$("#metrics").css(backgroundColor,"#d5c8e5");
-  });
-}
-
-document.addEventListener("DOMContentLoaded", onLoad);
-
 $(document).ready() {
 
   $("#metrics").addEventListener("focus", function(){
