08a996c58fcdfe48d05d818b58f6a2d414cd8d83
bwgaydon <bwgaydon@users.noreply.github.com>
2016-01-22 15:11:54 -0700

    Create script.js

---
+++ core/script.js
@@ -0,0 +1,15 @@
+function onLoad() {
+	$("#metrics").addEventListener("focus", function(){
+  	$("#metrics").css(backgroundColor,"#d5c8e5");
+  });
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
+
+$(document).ready() {
+
+  $("#metrics").addEventListener("focus", function(){
+  	$("#metrics").css(backgroundColor,"#d5c8e5");
+  });
+  
+}
