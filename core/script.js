2d2cf4928e4d499f2e286f37d76a46181894a159
bwgaydon <bwgaydon@users.noreply.github.com>
2016-01-24 10:58:05 -0700

    Update script.js

--- core/script.js
+++ core/script.js
@@ -1,8 +1,21 @@
+function contextSwitch(button) {
+	if(button.hasClass("selected")) return;
+  else {
+  	currentMenu.toggleClass("selected");
+    button.toggleClass("selected");
+    currentMenu = button;
+  }
+}
+
 $(document).ready(function() {
+	//global variables
+	currentMenu = $("#metrics_button");
+  currentMenu.addClass("selected");
+  
 	$(".menu_button").each(function() {
-  		this.addEventListener("click",function() {
-			$(this).toggleClass("selected");
-  	});
+  	this.addEventListener("click",function(){
+    	contextSwitch($(this));
+    });
   });
   
 });
