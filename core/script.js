8a5d6a19793631550589ab6d42686dfd40024982
bwgaydon <bwgaydon@users.noreply.github.com>
2016-02-15 17:20:08 -0700

    Update script.js

--- core/script.js
+++ core/script.js
@@ -7,6 +7,18 @@ function contextSwitch(button) {
     }
 }
 
+//get xyz from "variable=xyz"
+function getQueryVariable(variable)
+{
+   var query = window.location.search.substring(1);
+   var vars = query.split("&");
+   for (var i=0;i<vars.length;i++) {
+       var pair = vars[i].split("=");
+       if(pair[0] == variable){return pair[1];}
+   }
+   return(false);
+}
+
 $(document).ready(function() {
     //global variables
     currentMenu = $("#metrics_button");
@@ -18,5 +30,11 @@ $(document).ready(function() {
             contextSwitch($(this));
         });
     });
+    
+    //if redirect from oauth page, catch codes
+    if(window.location.search.substring(1) != "") {
+        var code = getQueryVariable("code");
+        console.log(code);
+    }
     
 });
