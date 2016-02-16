7091dc4ad9c054ef96ebdba49be70dfae2e8518f
bwgaydon <bwgaydon@users.noreply.github.com>
2016-02-16 15:15:36 -0700

    Update ski_script.js

--- ski/ski_script.js
+++ ski/ski_script.js
@@ -1,11 +1,14 @@
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 40.0076,
-            lng: -105.26594
-        },
-        zoom: 12
-    });
+	var myLatlng = new google.maps.LatLng(40.0076, -105.26594);
+	var mapOptions = {
+		zoom: 8,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.SATELLITE
+	};
+	
+	var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+	//markers: https://developers.google.com/maps/documentation/javascript/markers
+    
 }
 
 $(document).ready(function() {
