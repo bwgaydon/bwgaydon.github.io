812b2190d1ab7289c856a2018017d69299aa1b08
bwgaydon <bwgaydon@users.noreply.github.com>
2016-02-22 19:13:23 -0700

    Update ski_script.js

--- ski/ski_script.js
+++ ski/ski_script.js
@@ -1,7 +1,7 @@
 function initMap() {
 	
 	L.mapbox.accessToken = 'pk.eyJ1IjoiYndnYXlkb24iLCJhIjoiY2lreWxtd3U5MDJzcnZvbTNyeHlod2ZqZSJ9.oLn2gLt8kJu05nLxYkMBEQ';
-      	var map = L.mapbox.map('map', 'mapbox.streets')
+      	var map = L.mapbox.map('map', 'mapbox.outdoors')
       		.setView([39.567890,-106.1812467], 1);
       		
   		var featureLayer = L.mapbox.featureLayer()
