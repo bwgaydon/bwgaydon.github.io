16ca3d335360f74fd4f9720b17fd44503e5bdec1
bwgaydon <bwgaydon@users.noreply.github.com>
2016-03-06 17:45:00 -0700

    Update ski_script.js

--- ski/ski_script.js
+++ ski/ski_script.js
@@ -5,26 +5,26 @@ function initMap() {
       		.setView([39.567890,-106.1812467], 1);
       		
   	var featureLayer = L.mapbox.featureLayer()
-	        .loadURL('/ski/json/resorts.geojson')
+	        .loadURL('/json/resorts.geojson')
 	        .addTo(map);
         
     featureLayer.on('layeradd', function(e) {
 		var marker = e.layer,
 		feature = marker.feature;
 		marker.setIcon(L.icon(feature.properties.icon));
+		
+		var popupContent =  '<link href="//www.snow-forecast.com/stylesheets/feed.css" media="screen" rel="stylesheet" type="text/css" /><div id="wf-weatherfeed"><iframe style="overflow:hidden;border:none;" allowtransparency="true" height="272" width="469" src="//www.snow-forecast.com/resorts/Arapahoe-Basin/forecasts/feed/mid/m" scrolling="no" frameborder="0" marginwidth="0" marginheight="0"><p>Your browser does not support iframes.</p></iframe><div id="wf-link"><div style="clear: both;"></div></div></div>';
+
+    // http://leafletjs.com/reference.html#popup
+    marker.bindPopup(popupContent,{
+        closeButton: false,
+        minWidth: 499
+    });
 	});
     
 	featureLayer.on('ready', function() {
 		map.fitBounds(featureLayer.getBounds());
     	
-	});
-
-	
-	featureLayer.on('mouseover', function(e) {
-	    e.layer.openPopup();
-	});
-	featureLayer.on('mouseout', function(e) {
-	    e.layer.closePopup();
 	});
 
 }
