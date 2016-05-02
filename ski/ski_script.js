37c020ba41e345998465ae4bfe15c99c3e7b2780
bwgaydon <bwgaydon@users.noreply.github.com>
2016-05-02 15:50:29 -0600

    Update ski_script.js

--- ski/ski_script.js
+++ ski/ski_script.js
@@ -1,11 +1,10 @@
-function initMap() {
+function initMap(area) {
 	
 	L.mapbox.accessToken = 'pk.eyJ1IjoiYndnYXlkb24iLCJhIjoiY2lreWxtd3U5MDJzcnZvbTNyeHlod2ZqZSJ9.oLn2gLt8kJu05nLxYkMBEQ';
-      	var map = L.mapbox.map('map', 'mapbox.streets')
-      		.setView([39.567890,-106.1812467], 1);
+      	var map = L.mapbox.map('map', 'mapbox.streets');
       		
   	var featureLayer = L.mapbox.featureLayer()
-	        .loadURL('json/resorts.geojson')
+	        .loadURL('/json/'+ area +'.geojson')
 	        .addTo(map);
         
     featureLayer.on('layeradd', function(e) {
@@ -13,7 +12,7 @@ function initMap() {
 		feature = marker.feature;
 		marker.setIcon(L.icon(feature.properties.icon));
 		
-		var popupContent =  '<link href="//www.snow-forecast.com/stylesheets/feed.css" media="screen" rel="stylesheet" type="text/css" /><div id="wf-weatherfeed"><iframe style="overflow:hidden;border:none;" allowtransparency="true" height="272" width="469" src="//www.snow-forecast.com/resorts/' + feature.properties.urlTitle + '/forecasts/feed/mid/m" scrolling="no" frameborder="0" marginwidth="0" marginheight="0"><p>Your browser does not support iframes.</p></iframe><div id="wf-link"><div style="clear: both;"></div></div></div>';
+		var popupContent =  '<link href="//www.snow-forecast.com/stylesheets/feed.css" media="screen" rel="stylesheet" type="text/css" /><div id="wf-weatherfeed"><iframe style="overflow:hidden;border:none;" allowtransparency="true" height="272" width="469" src="//www.snow-forecast.com/resorts/' + feature.properties.title.replace(" ","-") + '/forecasts/feed/mid/m" scrolling="no" frameborder="0" marginwidth="0" marginheight="0"><p>Your browser does not support iframes.</p></iframe><div id="wf-link"><div style="clear: both;"></div></div></div>';
 
     // http://leafletjs.com/reference.html#popup
     marker.bindPopup(popupContent,{
@@ -24,7 +23,6 @@ function initMap() {
     
 	featureLayer.on('ready', function() {
 		map.fitBounds(featureLayer.getBounds());
-    	
 	});
 
 }
@@ -32,8 +30,18 @@ function initMap() {
 
 $(document).ready(function() {
 	
-	//epicMixWeather = $.getJSON("http://www.epicmix.com/vailresorts/sites/epicmix/api/mobile/weather.ashx");
-	
-	initMap();
+	$('.button').each(function() {
+		this.addEventListener("click", function() {
+			
+			initMap(this.id);
+			
+			$('#area_choice').css("opacity",0);
+             setTimeout(function(){
+                $('#area_choice').css("display","none");
+                $('#container').css("z-index",2);
+                $('#container').css("opacity",1);
+            }, 200);
+		})
+	})
 	
 });
