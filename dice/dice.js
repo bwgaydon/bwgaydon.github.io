5a0c6a3da3135531f7c0e3dde6d26825ed53f15d
unknown <bwgaydon@gmail.com>
2020-01-04 22:35:53 -0700

    sidebar link stuff

--- dice/dice.js
+++ dice/dice.js
@@ -1,22 +1,24 @@
 new Vue({
   el: '#app',
-  header: {
-    message: 'Hello Vue.js!'
-  },
-  sidebar: {
-  	items: [
-  	{
-  		linkText:"First",
-  		linkAddress:"http://google.com"
-  	},
-  	{
-  		linkText:"Second",
-  		linkAddress:"http://yahoo.com"
-  	},
-  	{
-  		linkText:"Third",
-  		linkAddress:"http://bing.com"
-  	}
-  	]
-  }
+  data: {
+	  header: {
+	    message: 'Hello Vue.js!'
+	  },
+	  sidebar: {
+	  	items: [
+	  	{
+	  		linkText:"First",
+	  		linkAddress:"http://google.com"
+	  	},
+	  	{
+	  		linkText:"Second",
+	  		linkAddress:"http://yahoo.com"
+	  	},
+	  	{
+	  		linkText:"Third",
+	  		linkAddress:"http://bing.com"
+	  	}
+	  	]
+	  }
+	}
 })
