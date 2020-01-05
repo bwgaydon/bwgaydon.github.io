1f8406e38f082045479a1655f4ec1c7774fd6178
unknown <bwgaydon@gmail.com>
2020-01-04 22:27:57 -0700

    sidebar links

--- dice/dice.js
+++ dice/dice.js
@@ -1,6 +1,22 @@
 new Vue({
   el: '#app',
-  data: {
+  header: {
     message: 'Hello Vue.js!'
+  },
+  sidebar: {
+  	items: [
+  	{
+  		linkText:"First",
+  		linkAddress:"http://google.com"
+  	},
+  	{
+  		linkText:"Second",
+  		linkAddress:"http://yahoo.com"
+  	},
+  	{
+  		linkText:"Third",
+  		linkAddress:"http://bing.com"
+  	}
+  	]
   }
 })
