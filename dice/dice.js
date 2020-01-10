fdcb98e2a59d9565f841a1fe222aa6d062d2c53e
unknown <bwgaydon@gmail.com>
2020-01-10 01:25:13 -0700

    infinite loop with filtering

--- dice/dice.js
+++ dice/dice.js
@@ -18,7 +18,7 @@ new Vue({
 		randomizeWeapons: function(weaponCount) {
 			// console.log('weaponCount: ',weaponCount);
 			// console.log('weaponData: ',weaponData);
-			console.log('weaponTypes: ',weaponTypes);
+			console.log('weaponTypes: ',formData.weaponTypes);
 			var weaponsArray = [];
 			for(var i=0;i<weaponCount;i++) {
 				var candidateWeapon = weaponData[Math.floor(Math.random() * weaponData.length)];
