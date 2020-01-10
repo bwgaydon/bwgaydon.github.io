4d38a009cc1f93b3650586b81ce52ff7aaa4f8ca
unknown <bwgaydon@gmail.com>
2020-01-10 15:13:36 -0700

    trying to tie together exclude type and individual

--- dice/dice.js
+++ dice/dice.js
@@ -30,7 +30,7 @@ new Vue({
 				for(var i=0;i<weaponCount;i++) {
 					var candidateWeapon = weaponData[Math.floor(Math.random() * weaponData.length)];
 					
-					if((this.formData.weaponTypes.length > 0 || this.formData.excludedWeapons.length > 0) && this.formData.excludedWeapons.length < weaponData.length - 4) {
+					if(this.formData.weaponTypes.length > 0 || this.formData.excludedWeapons.length > 0) {
 						while((!this.formData.weaponTypes.includes(candidateWeapon.weaponType) && this.formData.weaponTypes.length > 0) || weaponsArray.includes(candidateWeapon) || this.formData.excludedWeapons.includes(candidateWeapon.weaponName)) {
 							candidateWeapon = weaponData[Math.floor(Math.random() * weaponData.length)];
 						}
