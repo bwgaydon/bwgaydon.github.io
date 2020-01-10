6b5454150b123818426c576775b5894cf4c7b23a
unknown <bwgaydon@gmail.com>
2020-01-10 03:05:25 -0700

    pull yourself together man

--- dice/dice.js
+++ dice/dice.js
@@ -25,7 +25,9 @@ new Vue({
 			for(var i=0;i<weaponCount;i++) {
 				var candidateWeapon = weaponData[Math.floor(Math.random() * weaponData.length)];
 				
-				if(this.formData.weaponTypes.length > 0 || this.formData.excludedWeapons.length > 0) {
+				var weaponTypesPass = this.formData.weaponTypes.length > 0;
+				var excludedWeaponsPass = this.formData.excludedWeapons.length > 0;
+				if(weaponTypesPass || excludedWeaponsPass) {
 					while(!this.formData.weaponTypes.includes(candidateWeapon.weaponType) || weaponsArray.includes(candidateWeapon) || this.formData.excludedWeapons.includes(candidateWeapon.weaponName)) {
 						candidateWeapon = weaponData[Math.floor(Math.random() * weaponData.length)];
 					}
