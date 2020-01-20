c9cffa01641e821fe504428fa6f694f8f0e41fe9
unknown <bwgaydon@gmail.com>
2020-01-19 18:37:27 -0700

    trying to fix includeWeaponType function

--- dice/dice.js
+++ dice/dice.js
@@ -66,8 +66,6 @@ new Vue({
 			}
 		},
 
-		//IN DEVELOPMENT
-		//probably bad idea because of case where check category then do individual ones
 		includeWeaponType: function(type) {
 			console.log('this.formData.excludedWeapons: ',this.formData.excludedWeapons);
 			for(var i=this.formData.excludedWeapons.length - 1;i>=0;i--) {
